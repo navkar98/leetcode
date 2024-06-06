e783f291d3f8a0d9241e17cf38f5f072fa4a0f96
Navkar <jainnavkar01@gmail.com>
2024-06-06 15:09:37 -0400

    Time: 358 ms (38.65%), Space: 40.1 MB (5.24%) - LeetHub

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -0,0 +1,46 @@
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+
+        if n%k != 0:
+            return False
+
+        counter = {}
+        unique_card = set()
+
+        for i in nums:
+            if i not in counter:
+                counter[i] = 1
+                unique_card.add(i)
+            else:
+                counter[i] += 1
+
+        unique_card = list(unique_card)
+
+        heapq.heapify(unique_card)
+        curr_len = 0
+        last = inf
+
+        while unique_card:
+            # print(last, counter)
+            if last == inf:
+                while unique_card and counter[unique_card[0]] <= 0:
+                    heapq.heappop(unique_card)
+                
+                if unique_card:
+                    last = unique_card[0]
+                    counter[last] -= 1
+                    curr_len += 1
+            else:
+                if last + 1 not in counter or (last + 1 in counter and counter[last + 1] <= 0):
+                    return False
+                else:
+                    counter[last + 1] -= 1
+                    last = last + 1
+                    curr_len += 1
+
+            if curr_len == k:
+                curr_len = 0
+                last = inf
+
+        return True
