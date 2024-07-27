308faf1ee0a75f17273639ad6b217b00a020ef1d
Navkar <jainnavkar01@gmail.com>
2024-07-27 19:16:26 -0400

    Time: 853 ms (88.07%), Space: 18.2 MB (61.01%) - LeetHub

---
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        adj = collections.defaultdict(dict)
+
+        for f, t, c in zip(original, changed, cost):
+            if f in adj and t in adj[f] and adj[f][t] > c:
+                adj[f][t] = c
+            elif f not in adj or (f in adj and t not in adj[f]):
+                adj[f][t] = c
+
+        paths = {}
+        def explore_min_paths(node):
+            curr_paths = {node: 0}
+
+            heap = [(0, node)]
+
+            while heap:
+                curr_cost, curr_node = heapq.heappop(heap)
+
+                for t in adj[curr_node]:
+                    # Add path from root node to curr_node if cost is lower or NA
+                    if t not in curr_paths or (t in curr_paths and curr_paths[t] > curr_cost + adj[curr_node][t]):
+                        curr_paths[t] = curr_cost + adj[curr_node][t]
+                        # if t in paths:
+                        #     for tt in paths[t]:
+                        #         curr_sub_cost = curr_cost + paths[t][tt]
+                        #         if tt not in curr_paths or (tt in curr_paths and curr_paths[tt] > curr_sub_cost):
+                        #             curr_paths[tt] = curr_sub_cost
+                        # else:
+                        heapq.heappush(heap, (curr_cost + adj[curr_node][t], t))
+            
+            # print(node, curr_paths)
+            return curr_paths
+
+        for i in set(original):
+            paths[i] = explore_min_paths(i)
+
+        ans = 0
+        for i in range(len(source)):
+            if source[i] != target[i]:
+                if source[i] not in paths or (source[i] in paths and target[i] not in paths[source[i]]):
+                    return -1
+
+                ans += paths[source[i]][target[i]]
+
+        return ans
