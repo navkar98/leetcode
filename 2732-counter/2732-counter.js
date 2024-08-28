88aa03022516ca42a1b3855a54beaeab8c55477d
Navkar <jainnavkar01@gmail.com>
2024-08-28 14:15:57 -0400

    Time: 52 ms (47.27%), Space: 47.3 MB (99.85%) - LeetHub

---
+++ 2732-counter/2732-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var count = n;
+    return function() {
+        tmp = count;
+        count += 1
+        return tmp;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
